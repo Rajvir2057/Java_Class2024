7bad598c02a04384c8b88b31acb83fb98c6cdd36
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-03-26 11:25:54 +0300

    added homework

--- User_input.java
+++ User_input.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class User_input{
     public static void main(String[] args){
         Scanner Input = new Scanner(System.in);
+
         System.out.print("Enter your name: ");
         String Name = Input.nextLine();
+
         System.out.print("Enter your age: ");
         int Age= Input.nextInt();
+        
         System.out.println("Your name is: " + Name + ", Your age is: " + Age);
     }
 }
---
+++ assignments/assignment4/Addition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+// this wil let a user to input 2 numbers that will add up;
+public class Addition{
+    public static void main(String[] args){
+        Scanner addition = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int add1 = addition.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int add2 = addition.nextInt();
+
+        int sum = add1 + add2;
+
+        System.out.println("The sum of "+ add1 + " and "+ add2 + " is = "+ sum );
+    }
+}
