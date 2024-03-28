f4dcc607c4fb4e2def6ffcdd85a3cd0f1da15cca
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-03-28 15:42:25 +0300

    classwork

---
+++ loops/Forloop.java
@@ -0,0 +1,15 @@
+
+public class Forloop {
+    public static void main(String[] args) {
+
+        for(int i=0; i >= 10; i--){
+            System.out.println(i);
+        }
+        for(int x =0; x<= 10; x++ ){
+            if(x%2 !=0){
+                System.out.println(x);
+            }
+        }
+    }
+    
+}
