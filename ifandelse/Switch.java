5dac2c46d3dad5b2a077c763829bcd4e504d3261
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-04-04 16:18:08 +0300

    kk

---
+++ ifandelse/Switch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Switch { 
+
+    public static void main(String[] args) {
+
+        Scanner ageinput = new Scanner(System.in);    
+
+        System.out.print("Enter your age please: ");
+        int age = ageinput.nextInt();
+
+        switch (age) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            System.out.println("Infant");
+            break;
+            case 30:
+            case 40:
+            case 50:
+            System.out.println("youth");
+            break;
+            case 60:
+            case 70:
+            System.out.println("Old");
+        }
+    }
+
+
+
+    
+}
