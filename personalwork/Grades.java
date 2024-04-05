396f7042c8a8f05d9224374d6af42e394eba8ade
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-04-05 12:27:23 +0300

    test

---
+++ personalwork/Grades.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Grades {
+    public static void main(String[] args) {
+        
+        Scanner scanneddata = new  Scanner (System.in);
+
+        System.out.print("Enter a number from 65 eg 65 prints A: ");
+        int grade = scanneddata.nextInt();
+
+        switch(grade){
+            case 65:
+            System.out.println("you got an: " +(char)grade);
+            break;
+            case 66:
+            System.out.println("you got a: " + (char)grade);
+            break; 
+            case 67:
+            System.out.println("you got a: " + (char)grade);
+            break;
+        }
+        scanneddata.close();
+    }
+}
